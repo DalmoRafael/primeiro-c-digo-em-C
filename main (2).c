26ef296d4a1ea4937570fbb3546c0951733f2f9e
DalmoRafael <104334002+DalmoRafael@users.noreply.github.com>
2024-07-23 10:53:32 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int x=5;
+    x++;
+    
+    
+    printf("incremento %d \n", x);
+    
+    x--;
+    
+    
+    printf("decremento %d \n", x);
+    
+    
+    x--;
+    
+    
+    printf("decremento %d", x);
+    
+    
+
+    
+    
+    return 0;
+}
